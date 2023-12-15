e7baf664da71c03cec498ba15b9a3f710d955eff
Shubham <126546303+shubhamHastree@users.noreply.github.com>
2023-12-15 17:41:55 +0530

    Initial Commit

---
+++ TableViewDemo/TableViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewDemoApp.swift
+//  TableViewDemo
+//
+//  Created by Apps WeLove on 15/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TableViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
